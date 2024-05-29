696a8a697dfa8d822e9d0eee5c3c711dab4c9b88
Estrella <Estrella@MELI>
2024-05-29 17:08:24 -0600

    Agregar archivos de proyecto.

---
+++ 10195594Video5/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace _10195594Video5
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        ///  El evento clicked del botón lo que realiza es que cambia el texto que estába por otro dentro del botón
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Presioname de nuevo";
+        }
+    }
+
+}
